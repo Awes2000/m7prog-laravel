02b7fc6a68edf5a4c041f7bcd0a8f3a38fe746af
Awes <28003@ma-web.nl>
2024-04-12 10:36:48 +0200

    controllers about page gemaakt

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/youtube', function () {
+    return redirect()->away('https://www.youtube.com');
+
+
 });
+
+
+
+});
+    Route::get('/about', [AboutController::class, 'index'])->name('about');
+
 
 require __DIR__.'/auth.php';
